b876ed5297365f7d86fe280812a735559e0ae5ab
vinnieOrdobas <viniordobas@icloud.com>
2020-12-31 14:59:29 +0000

    main.js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,3 @@ function findRecipe(data) {
     recipeList.innerHTML = searchString;
 }
 
-
-/* const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ranking=1&ignorePantry=true",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-}); */
-

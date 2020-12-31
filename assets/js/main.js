2968e290c003b7a9e60c7e68d1f3bf7e44874aa1
vinnieOrdobas <viniordobas@icloud.com>
2020-12-31 07:22:30 +0000

    fuctions updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,26 @@ const recipeList = document.getElementById('recipeList');
 const searchBar = document.getElementById('searchBar');
 const buttonSubmit = document.getElementById('buttonSubmit');
 
-buttonSubmit.addEventListener('click', myFunction);
+buttonSubmit.addEventListener('click', findRecipe);
 
-function myFunction(data) {
-    const searchString = searchBar.value + " " + "vegan";
-    console.log(searchString);
+function findRecipe(data) {
+    const searchString = searchBar.value.toLowerCase() + " " + "vegan";
+    recipeList.innerHTML = searchString;
 }
 
+
+/* const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ranking=1&ignorePantry=true",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "13b8334a45mshc2f5b45765f960cp1ea18ajsnb4cf78ea6aab",
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+}); */
 

abc0c100ea4c823821140a7724d2dc7f0226bc0e
vinnieOrdobas <viniordobas@icloud.com>
2021-01-01 07:46:31 +0000

    function updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,11 @@ const buttonSubmit = document.getElementById('buttonSubmit');
 const buttonAPI = document.getElementById('buttonAPI');
 const myRequest = new XMLHttpRequest();
 
+
 buttonSubmit.addEventListener('click', findRecipe);
 
 
-buttonAPI.addEventListener('click', makeRequest);
+buttonAPI.addEventListener('click', makeRequest(printToHTML));
 
 
 function findRecipe(data) {
@@ -15,23 +16,26 @@ function findRecipe(data) {
     recipeList.innerHTML = searchString;
 }
 
-function makeRequest() {
+function printToHTML(data) {
+    recipeList.innerHTML = data[0].image;
+}
+
+function makeRequest(cb) {
     myRequest.open('GET',"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ranking=1&ignorePantry=true");
     myRequest.setRequestHeader("x-rapidapi-key", "a");
     myRequest.setRequestHeader("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
 
-    var data = JSON.parse(ourRequest.responseText);
 
     myRequest.onload = function() {
-        if (myRequest.status != 200) {
-            console.log('An error occurred');
+        if (this.readyState == 4 && this.status == 200) {
+           cb(JSON.parse(myRequest.responseText));
         } else {
-            console.log(myRequest.responseText);
+            console.log('An error occurred');
         }
     }
+
 
     myRequest.send()
 };
 
 
-function printToHTML(data);

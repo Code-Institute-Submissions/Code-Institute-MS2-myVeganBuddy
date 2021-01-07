f0d80769a8927f0a87b35c44bc9cbb6c7b25cf42
vinnieOrdobas <viniordobas@icloud.com>
2021-01-07 14:04:40 +0000

    renderRecipe function created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,45 @@
+
+// Declaring constants
 const url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex"
 const buttonApi = document.getElementById('buttonApi');
 const recipeList = document.getElementById('recipeList');
+const recipeRender = document.getElementById('recipeRender');
+
+// Declaring event listeners
 buttonApi.addEventListener('click', requestAPI);
 
 
-function requestAPI() {
 
+function requestAPI() {
+    // Gets the search query and passes as a parameter to the Request
     var searchString = document.getElementById('searchBar').value;
 
     fetch(`${url}?limitLicense=true&offset=0&number=10&diet=vegan&includeIngredients=${searchString}&ranking=2&maxCalories=1500&maxFat=100&maxProtein=100&maxCarbs=100&fillIngredients=false&instructionsRequired=false&addRecipeInformation=true`, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "13b8334a45mshc2f5b45765f960cp1ea18ajsnb4cf78ea6aab",
+            "x-rapidapi-key": "a",
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
         }
     })
+        // Transform the response into a json object
         .then((response) => response.json())
         .then((data) => renderResponse(data))
-        .catch(err => {
-            alert(err);
-        });
+        .catch(err => alert(err));
 }
 
-
+// Renders the JSON object in the HTML placeholder element
 function renderResponse(data) {
-    let output = `<h2 class="title text-center">Results</h2>`;
+    let output = `
+    
+    <div class="col">
+        <h2 class="title text-center">Results</h2>`;
+    // Accesses the array
     data = data.results;
+    // Loops through the array of recipes and renders them separately 
     data.forEach(function(recipe) {
         output +=
             
-            `<div class="col">
+            `
                 <div class="card">
                     <img src="${recipe.image}" class="card-img-top" alt="${recipe.title}">
                     <div class="card-body">
@@ -50,18 +60,58 @@ function renderResponse(data) {
                 </div>
             </div>`
     });
+    // Renders the above template into the target div element
     recipeList.innerHTML = output;
 };
 
+
+// Passes the ID property as a parameter to request the particular recipe and renders to the HTML placeholder element
 function requestRecipe(recipeId) {
-    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/information`, {
+    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/information?includeNutrition=true`, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "x",
+            "x-rapidapi-key": "a",
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
         }
     })
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => renderRecipe(data))
         .catch((err) => alert(err));
 };
+
+
+function renderRecipe(data){
+    // access the nutrients property of the object
+    let nutrients = data.nutrition.nutrients; 
+    // Creates variable with HTML elements to render on the page
+    let recipeOutput =  `
+    <div class="row">    
+        <div class="col-12 text-center">
+            <img src="${data.image}" class="img-thumbnail" alt="${data.title}">
+            <h2>${data.title}</h2>
+        </div>
+        <p><strong>Summary:</strong>${data.summary}</p><br>
+        <h3 class="text-center"> Macros: </h3><br>
+        <ul class="recipe-detail">
+            <li>Calories: ${nutrients[0].amount} kcal</li>
+            <li>Fat: ${nutrients[1].amount} grams</li>
+            <li>Carbs: ${nutrients[3].amount} grams</li>
+            <li>Protein: ${nutrients[8].amount} grams</li>
+        </ul>
+    </div>`
+    // Accesses the ingredients property of the object
+    let ingredients = data.extendedIngredients
+    let ingredientOutput = `
+    <h2> Ingredients:</h2>
+    <ul class=ingredient-detail>`
+    // Loops through the ingredients and prints it to the page
+    ingredients.forEach(function(ingredient) {
+        ingredientOutput +=
+            `<li>${ingredient.amount} ${ingredient.unit} ${ingredient.name}</li>`
+         
+    });
+    // Re-defeines the variable and adds the closing HTML tag
+    ingredientOutput = ingredientOutput + `</ul>`
+    // Directs the variable to the html element
+    recipeRender.innerHTML = recipeOutput + ingredientOutput;
+}

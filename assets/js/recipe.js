95a22656bf51b5ba00a06510f019f0cb14de9c15
Vinicius Ordobas Muniz <vmuniz@fb.com>
2021-01-20 12:40:06 +0000

    renderRecipe updated with right order of HTML elements

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -31,35 +31,59 @@ function renderRecipe(data) {
                     <img src="${data.image}" class="img-fluid img-responsive rounded" alt="${data.title}">
                 </div>
             </div>`
-    let macros = 
+    
+    // How to create list groups with icons - Code found on https://mdbootstrap.com/snippets/jquery/mdbootstrap/949128#html-tab-view    
+        let macros = 
         `
             <div class="col-sm-12 col-md-6">
                 <div class="card text-center">
-                    <div class="card-body">
+                    <div class="card-header">
                         <div class="title-wrapper text-center">
-                            <h3 class="card-title recipe-title">${data.title}</h3>
+                                <h4 class="card-title recipe-title">${data.title}</h4>
                         </div>
-                        <div class="icons-wrapper">
-                            <div class="stars-outer">
-                                <div class="stars-inner" style="width:${rating}%"></div>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="card-body">
                         <div class="recipe-info">
-                            <ul class="list-group">
-                                <li class="list-item"><i class="far fa-clock"></i> - ${data.readyInMinutes} minutes</li>
-                                <li class="list-item"><i class="fas fa-thumbs-up"> - ${data.aggregateLikes}</i></li>
-                                <li class="list-item"> WW SmartPoints - ${data.weightWatcherSmartPoints}</li> 
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div>
+                                    <div class="stars-outer">
+                                        <div class="stars-inner" style="width:${rating}%"></div>
+                                    </div>
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div><i class="far fa-clock"></i> - ${data.readyInMinutes} minutes
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div><i class="fas fa-thumbs-up"></i> - ${data.aggregateLikes} likes
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div><i class="fas fa-cookie-bite"></i> - ${nutrients[0].amount.toFixed(1)} kcal
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div><i class="fas fa-weight"></i> WW SmartPoints - ${data.weightWatcherSmartPoints} points
+                                 </li>
+                                 <li class="list-group-item">
+                                    <div class="md-v-line"></div><i class="fas fa-users"></i> - ${data.servings} portion(s)
+                                </li>
                             </ul>
                         </div>
                         <h5 class="card-title"> Macros: </h5>
-                        <div class="card-text">
-                            <ul class="macros-simple">
-                                <li>Calories: ${nutrients[0].amount.toFixed(1)} kcal </li>
-                                <li>Fat: ${nutrients[1].amount.toFixed(1)} grams </li>
-                                <li>Carbs: ${nutrients[3].amount.toFixed(1)} grams </li>
-                                <li>Protein: ${nutrients[8].amount.toFixed(1)} grams </li> 
+                        <div class="macros-wrapper">
+                            <ul class="list-group-item list-group-flush">
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div> - Fat: ${nutrients[1].amount.toFixed(1)} grams
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div> - Carbs: ${nutrients[3].amount.toFixed(1)} grams
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div> - Protein: ${nutrients[8].amount.toFixed(1)} grams
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="md-v-line"></div><button type="button" class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#table-collapse">Full List of Nutrients</button>
+                                </li>
                             </ul>
-                            <button class="btn btn-primary" data-toggle="collapse" data-target="#table-collapse">Full List of Nutrients</button>
                         </div>
                     </div>  
                 </div>
@@ -110,8 +134,7 @@ function renderRecipe(data) {
     let instructionOutput = 
    
     `   
-    <div class="row">
-    <div class="col-md-8 method-list"> 
+    <div class="col-md-8 order-md-1 method-list"> 
         <h3 class="section-title"> Method: </h3>
         <ol>`
 
@@ -127,30 +150,31 @@ function renderRecipe(data) {
     instructionOutput = instructionOutput + 
 
     `
-         </ol>
+            </ol>
+        </div>
     </div>`
 
     // Accesses the ingredients property of the object
     let ingredients = data.extendedIngredients
     let ingredientOutput = `
-    <div class="col-md-4 order-1 ingredient-list">
-        <h2> Ingredients:</h2>
-        <ul class=ingredient-detail>`
+    <div class="row">
+        <div class="col-md-4 order-md-12 ingredient-list">
+            <h2> Ingredients:</h2>
+            <ul class="ingredient-detail">`
     // Loops through the ingredients and prints it to the page
     ingredients.forEach(function (ingredient) {
         ingredientOutput +=
            
-        `<li class="list-item">${ingredient.amount} ${ingredient.unit} ${ingredient.name}</li>`
+        `<li class="list-item">${ingredient.amount.toFixed(1)} ${ingredient.unit} ${ingredient.name}</li>`
 
     });
     // Re-defines the variable and adds the closing HTML tag
     ingredientOutput = ingredientOutput +
     
     `       </ul>
-        </div>
-    </div>`
+        </div>`
     // Directs the variable to the html element
-    var requestedRecipe = recipeOutput + macros + instructionOutput + ingredientOutput;
+    var requestedRecipe = recipeOutput + macros + ingredientOutput + instructionOutput; // Swapping column orders for different screen sizes [How to Reorder Columns with Bootstrap 4 Order Classes] - Code found on: https://bootstrapcreative.com/bootstrap-push-pull-column-ordering-tutorial/
     localStorage.setItem('requestedRecipe', requestedRecipe);
     window.location.href = "recipe.html";
 }
